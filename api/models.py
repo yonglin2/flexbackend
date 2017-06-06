378e14c15ca02a1e6652095087d2d9fa707fb7cb
Yong Lin <gmale88@gmail.com>
2017-06-06 11:47:43 -0700

    Prep for restaurant migration

--- api/models.py
+++ api/models.py
@@ -3,15 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Restaurant(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, default='')
     place_id = models.IntegerField()
+    user_likes = models.ManyToManyField(User, related_name='restaurant_likes')
 
+# class Like(models.Model):
+#     user = models.ForeignKey(User)
+#     restaurant = models.ForeignKey(Restaurant)
+#     created = models.DateTimeField(auto_now_add=True)
+#
+# class Dislike(models.Model):
+#     user = models.ForeignKey(User)
+#     restaurant = models.ForeignKey(Restaurant)
+#     created = models.DateTimeField(auto_now_add=True)
 
-class Like(models.Model):
-    user = models.ForeignKey(User)
-    picture = models.ForeignKey(Picture)
-    created = models.DateTimeField(auto_now_add=True)
-    
 # Create your models here.
