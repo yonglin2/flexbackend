dcd871b6480423e0e229f9ddb99260f861cad726
Yong Lin <gmale88@gmail.com>
2017-06-06 11:19:29 -0700

    Create restaurant and like model

--- api/models.py
+++ api/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Restaurant(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, default='')
+    place_id = models.IntegerField()
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User)
+    picture = models.ForeignKey(Picture)
+    created = models.DateTimeField(auto_now_add=True)
+    
 # Create your models here.
